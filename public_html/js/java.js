555bdc5c6a314b524f62086b17ce19ea5c789256
liang799 <71926348+liang799@users.noreply.github.com>
2021-11-11 11:13:16 +0800

    added files

---
+++ public_html/js/java.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+alert("Hello! I am an annoying alert box!!");
+alert("Holla!! I am back!!!");
+
+document.write(5+6);
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+
